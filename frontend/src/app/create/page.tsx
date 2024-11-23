776929d9d2d596744b0c31cbf78e635a9e35c586
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-23 19:21:29 +0900

    frontend: create 페이지 UI 구현

--- frontend/src/app/create/page.tsx
+++ frontend/src/app/create/page.tsx
@@ -1,3 +1,10 @@
+import PinCreationForm from '@/components/create/PinCreationForm';
+
 export default function Create() {
-  return <div>Create</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mt-4">핀 만들기</h1>
+      <PinCreationForm className="mt-4" />
+    </div>
+  );
 }
