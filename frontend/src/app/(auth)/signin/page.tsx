867b63413f19273fb65766b911af2768d54157d5
kimtaejin3 <rlaxowls1316@naver.com>
2024-11-24 21:04:35 +0900

    frontend: Signin 페이지 UI 구현

--- frontend/src/app/(auth)/signin/page.tsx
+++ frontend/src/app/(auth)/signin/page.tsx
@@ -1,3 +1,35 @@
+import Link from 'next/link';
+
 export default function Page() {
-  return <div>Signin</div>;
+  return (
+    <form>
+      <div>
+        <label htmlFor="nickname">닉네임</label>
+        <input
+          className="mt-2 border-[1px] border-slate-300 w-full rounded-2xl px-4 py-2"
+          type="text"
+          id="nickname"
+          placeholder="닉네임을 입력해주세요"
+        />
+      </div>
+      <div className="mt-8">
+        <label htmlFor="password">비밀번호</label>
+        <input
+          className="mt-2 border-[1px] border-slate-300 w-full rounded-2xl px-4 py-2"
+          type="text"
+          id="password"
+          placeholder="비밀번호를 입력해주세요"
+        />
+      </div>
+
+      <div className="mt-8">
+        <button className="w-full shrink-0 bg-[#e60021] h-9 text-white px-4 rounded-xl">
+          로그인
+        </button>
+        <Link className="mt-2 block text-center text-sm" href="/signup">
+          회원가입
+        </Link>
+      </div>
+    </form>
+  );
 }
