b0a08734b37ac82622e3f9f3d2cded5c034c783f
kimtaejin3 <rlaxowls1316@naver.com>
2024-12-08 17:02:45 +0900

    frontend: backend getAllImages return 값 변경에 따른 수정

--- frontend/src/components/shared/Cards.tsx
+++ frontend/src/components/shared/Cards.tsx
@@ -18,7 +18,7 @@ export default function Cards({ className }: { className?: string }) {
     <div
       className={`columns-2 md:columns-4 space-y-5 gap-4 min-h-[1400px] ${className}`}
     >
-      {data?.map((elem: any, index: number) => (
+      {data?.images.map((elem: any, index: number) => (
         <Card key={index} cover={elem.image_url} {...elem} />
       ))}
     </div>
