6376078438bd467ff46702eca9b65b5d635adbd5
kimtaejin3 <rlaxowls1316@naver.com>
2024-12-13 19:44:44 +0900

    frontend: axios.create 수정

--- frontend/src/apis/index.ts
+++ frontend/src/apis/index.ts
@@ -2,7 +2,10 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: "https://port-0-grinda-rest-m4jhu7695910b72b.sel4.cloudtype.app",
-  timeout: 1000,
+  timeout: 5000,  // 5초로 증가
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
 export default api;
