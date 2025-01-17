12555fc739db95181d14f49256b8c718b7899eeb
kimtaejin3 <rlaxowls1316@naver.com>
2025-01-17 16:47:27 +0900

    frontend: 비교를 위한 suspense 제거

--- frontend/src/app/(notAuth)/page.tsx
+++ frontend/src/app/(notAuth)/page.tsx
@@ -1,7 +1,5 @@
-import { Suspense } from 'react';
 
 import CardsHydrate from '@/components/shared/CardsHydrate';
-import CardsLoading from '@/components/shared/CardsLoading';
 import ProgressWrap from '@/components/shared/ProgressWrap';
 
 export default async function Home({
@@ -15,13 +13,7 @@ export default async function Home({
   return (
     <div className="pb-40">
       <ProgressWrap />
-      <Suspense
-        fallback={
-          <CardsLoading className="mt-3" columns={4} itemsPerColumn={5} />
-        }
-      >
-        <CardsHydrate page={page} search={search} />
-      </Suspense>
+      <CardsHydrate page={page} search={search} />
     </div>
   );
 }
--- frontend/src/hooks/useLoadingDelay.ts
+++ frontend/src/hooks/useLoadingDelay.ts
@@ -1,16 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function useLoadingDelay(dependencies: any[], delay: number) {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const isFirstRender = useRef(true);
 
   useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
     setIsLoading(true);
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, delay);
     return () => clearTimeout(timer);
-  }, [delay, ...dependencies]);
+  }, [delay, isFirstRender, ...dependencies]);
 
   return isLoading;
 }
