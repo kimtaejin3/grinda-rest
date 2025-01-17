cf1f81ea5fc82eb7d04ec2336f3c732b476f5043
kimtaejin3 <rlaxowls1316@naver.com>
2025-01-17 20:53:39 +0900

    frontend(fix): useLoadingDelay.ts
    - 첫 렌더링 시에는 딜레이 없도록 수정

--- frontend/src/hooks/useLoadingDelay.ts
+++ frontend/src/hooks/useLoadingDelay.ts
@@ -1,16 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function useLoadingDelay(dependencies: any[], delay: number) {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const isFirstRender = useRef(true);
 
   useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      console.log('isFirstRender');
+      return;
+    }
+    console.log('ii')
     setIsLoading(true);
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, delay);
     return () => clearTimeout(timer);
-  }, [delay, ...dependencies]);
+  }, [...dependencies]);
 
   return isLoading;
 }
